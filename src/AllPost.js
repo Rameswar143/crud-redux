2e7a4c1ac852e775b69a32a6bbc7117aea3f94e0
PRAMESWA <ramu08078@gmail.com>
2019-12-27 21:59:50 +0530

    first commit

---
+++ src/AllPost.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+class AllPost extends Component {
+  render() {
+    return (
+      <div>
+        <h1>All Posts</h1>
+        {console.log(this.props.posts)}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log("Hi Ramu  " + new Date());
+  return {
+    posts: state
+  }
+}
+export default connect(mapStateToProps)(AllPost);
